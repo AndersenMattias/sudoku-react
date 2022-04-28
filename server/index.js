14fb8af946e64f2a6cb2f79d8f9e7e56f1d1320c
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-04-28 18:10:46 +0200

    Changes for heroku

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const cors = require('cors');
 const express = require('express');
 // Loads env variables
 require('dotenv').config()
@@ -7,13 +6,12 @@ require('dotenv').config()
 const gameRouter = require('./routes/game');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
 
 app.use(express.json());
-app.use(cors());
 
 app.use(gameRouter)
 
