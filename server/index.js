61fef233acc572ee322a1a12f1474b4fa61b0f69
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-04-29 10:56:39 +0200

    Changes for heroku

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
+
 // Loads env variables
 require('dotenv').config()
 
 const gameRouter = require('./routes/game');
+const PORT = process.env.PORT || 5000;
+
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
---
+++ src/SodukoHandler.js
@@ -0,0 +1,58 @@
+import {SUDOKU} from '../src/components/Api/api'
+
+// fill gridArray with numbers
+    export function GridLayout() {
+    const grid = [];
+    for (let i = 0; i < 9; i++) {
+      grid[i] = Array(9).fill(0);
+    }
+    return grid;
+  }
+  
+  
+  export function copyArrFromTo(from, to) {
+    for (let i = 0; i < from.length; i++) {
+      to[i] = [...from[i]];
+    }
+  }
+
+  // Call soduko board depending on which btn choosen
+  export async function onCreateNewGame(lvl) {     
+    switch (lvl) {
+      case 1:
+        try {
+          const response = await SUDOKU.beginnerBoard();
+          const data = await response.json();
+          return data.game;
+        } catch (error) {
+          console.log(error);
+        }
+        break;
+
+      case 2:
+        try {
+          const response = await SUDOKU.intermediateBoard();
+          const data = await response.json();
+          return data.game;
+        } catch (error) {
+          console.log(error);
+        }
+        break;
+
+      case 3:
+        try {
+          const response = await SUDOKU.masterBoard();
+          const data = await response.json();
+          return data.game;
+        } catch (error) {
+          console.log(error);
+        }
+        break;
+        default:
+          throw new Error("Invalid action");
+    }   
+  }
+
+
+
+
